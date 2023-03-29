280b1380dc658a02623dc760264fa392f2fd7503
Yohann DC <YohannDCz@gmail.com>
2023-03-29 17:20:39 +0200

    Auto stash before revert of "Debug Navbar"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:indexed/indexed.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,18 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Devjobs',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: const Color(0xff5964E0)),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -48,82 +39,128 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final logotype = AssetImage('assets/logos/logo.svg');
+  get decoration => null;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        toolbarHeight: 136,
-        flexibleSpace: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/background.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 24, top: 32, right: 24, bottom: 72),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Image(
-                    image: logotype,
-                    width: 200,
-                  ),
-                  Row(children: [
-                    Image(image: AssetImage('assets/')),
-                  ]),
-                ],
-              ),
-            ),
+      body: Indexer(children: const [
+        Indexed(
+          index: 10,
+          child: Positioned(
+            child: appBar(),
           ),
         ),
+        Indexed(
+          index: 100,
+          child: Positioned(
+  }
+}
+
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      width: 327,
+      color: Colors.amber,
+      child: Column(
+        children: [
+          Stack(
+            children: [Container(
+              height: 80,
+              transform: Matrix4.translationValues(0.0, 160.0, 0.0),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+              // ignore: prefer_const_constructors
+              child: TextField(
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Filter by title...',
+                  contentPadding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
+                ),
+              ),
+            ),
+        ]),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    ));
+  }
+}
+
+// ignore: camel_case_types
+class appBar extends StatelessWidget {
+  const appBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // Here we take the value from the MyHomePage object that was created by
+      // the App.build method, and use it to set our appbar title.
+
+      height: 200,
+      child: Stack(children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: SvgPicture.asset(
+            'assets/mobile/bg-pattern-header.svg',
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Padding(
+          padding: const EdgeInsets.only(left: 24, top: 96, right: 24, bottom: 72),
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            SvgPicture.asset(
+              'assets/desktop/logo.svg',
+              fit: BoxFit.cover,
+            ),
+            Row(children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: SvgPicture.asset(
+                  'assets/desktop/icon-sun.svg',
+                ),
+              ),
+              const SwitchExample(),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: SvgPicture.asset(
+                  'assets/desktop/icon-moon.svg',
+                ),
+              ),
+            ]),
+          ]),
+        ),
+      ]),
+    );
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: light,
+      onChanged: (bool value) {
+        setState(() {
+          light = value;
+        });
+      },
+      activeColor: const Color(0xff5964E0),
+      inactiveThumbColor: const Color(0xff5964E0),
+      inactiveTrackColor: Colors.white,
+      activeTrackColor: Colors.white,
     );
   }
 }
